8a503ea90d8cd71600c48445814dd8d7fb3a8edd
Derek Myers <arcticpro@gmail.com>
2019-10-04 22:58:20 -0500

    custom validation rules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Custom substring extension of alpha rule
+        Validator::extend('alpha_start', function ($attribute, $value, $parameters, $validator) {
+            $length = $parameters[0];
+            $startPos = 0;
+            $endPos = $startPos + $length;
+            $valueStart = substr($value, $startPos, $endPos);
+            return $validator->validateAlpha($attribute, $valueStart);
+        });
+
+        // Custom substring extension of numeric rule
+        Validator::extend('numeric_end', function ($attribute, $value, $parameters, $validator) {
+            $length = $parameters[0];
+            $startPos = (strlen($value) - $length);
+            $endPos = strlen($value);
+            $valueEnd = substr($value, $startPos, $endPos);
+            return $validator->validateNumeric($attribute, $valueEnd);
+        });
     }
 }
