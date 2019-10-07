f847fe76427d8fb31a793fa9f32265d897e32d6c
Derek Myers <arcticpro@gmail.com>
2019-10-06 21:14:27 -0500

    app

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>USCIS Case Tracker</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app"></div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('app', function () {
+    return view('app');
+});
