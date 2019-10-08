5a6d2fd3862b854a3d34738a52d3cf5c5fd3bbce
Derek Myers <arcticpro@gmail.com>
2019-10-08 14:22:01 -0500

    cleanup

--- resources/js/components/AddCaseModal.vue
+++ resources/js/components/AddCaseModal.vue
@@ -1,6 +1,6 @@
 <template>
     <b-modal @shown="focusInput" ref="modal" id="modal-add-case" title="Add Case" buttonSize="lg" okTitle="Add Case" centered>
-        <b-form @submit.stop.prevent="onFormSubmit">
+        <b-form @submit.stop.prevent="onFormSubmit" ref="form">
             <b-form-group label="Case Number" label-for="case-number" label-size="lg" description="The receipt number for your case from USCIS.">
                 <b-form-input v-model="caseId" type="text" size="lg" ref="caseNumber" id="case-number" placeholder="ABC1234567890" required />
             </b-form-group>
