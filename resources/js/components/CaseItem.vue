a90279f73e1e35eaa2d6ea69504e49ce29efe357
Derek Myers <arcticpro@gmail.com>
2019-10-07 21:41:48 -0500

    component cleanup

--- resources/js/components/CaseItem.vue
+++ resources/js/components/CaseItem.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-    </div>
+    <b-card header-tag="header" footer-tag="footer" class="mb-3">
+        <template v-slot:header>
+            <h5 class="float-left card-heading mb-0">
+                <span class="text-muted"><strong>Case</strong> #</span>{{ item.id }}
+            </h5>
+
+            <b-badge class="float-right">
+                <span class="case-title">
+                    {{ item.title }}
+                </span>
+            </b-badge>
+
+            <div class="clearfix"></div>
+        </template>
+
+        <b-card-text>
+            <p class="m-0 p-0">
+                {{ item.details }}
+            </p>
+        </b-card-text>
+
+        <template v-slot:footer>
+            <div class="text-center text-muted">
+                <small>
+                    Last Updated: {{ item.date | moment("dddd, MMMM Do YYYY") }}
+                </small>
+            </div>
+        </template>
+    </b-card>
 </template>
 
 <script>
--- resources/js/components/CaseList.vue
+++ resources/js/components/CaseList.vue
@@ -26,35 +26,7 @@
                 <div class="clearfix"></div>
 
                 <div id="cases" class="mt-4">
-                    <b-card v-for="(item, key) in cases" :key="key" header-tag="header" footer-tag="footer" class="mb-3">
-                        <template v-slot:header>
-                            <h5 class="float-left card-heading mb-0">
-                                <span class="text-muted"><strong>Case</strong> #</span>{{ item.id }}
-                            </h5>
-
-                            <b-badge class="float-right">
-                                <span class="case-title">
-                                    {{ item.title }}
-                                </span>
-                            </b-badge>
-
-                            <div class="clearfix"></div>
-                        </template>
-
-                        <b-card-text>
-                            <p class="m-0 p-0">
-                                {{ item.details }}
-                            </p>
-                        </b-card-text>
-
-                        <template v-slot:footer>
-                            <div class="text-center text-muted">
-                                <small>
-                                    Last Updated: {{ item.date | moment("dddd, MMMM Do YYYY") }}
-                                </small>
-                            </div>
-                        </template>
-                    </b-card>
+                    <case-item v-for="(item, key) in cases" :key="key" :item="item" />
                 </div>
             </b-col>
         </b-row>
