68b4765a1f3dc5bf85423368b8daa8b0f27bd545
Derek Myers <arcticpro@gmail.com>
2019-10-07 19:49:10 -0500

    booststrap vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,11 @@
 
 require('./bootstrap');
 
-require('bootstrap-vue');
+const BootstrapVue = require('bootstrap-vue');
 
 window.Vue = require('vue');
+
+Vue.use(BootstrapVue);
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
