bdfbfa3794f3988d7d5e0b8b9acea8bb6f8228fb
Derek Myers <arcticpro@gmail.com>
2019-10-08 19:49:09 -0500

    favicon webpack plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,6 +13,15 @@ const mix = require('laravel-mix');
  */
 
 mix.disableNotifications();
+
+mix.webpackConfig({
+    plugins: [
+        new FaviconsWebpackPlugin({
+            logo: './resources/img/logo.png',
+            prefix: 'img/'
+        }),
+    ]
+});
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
