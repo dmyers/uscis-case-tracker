d64e8dc7e1b281cd4346e04f3d22aa082507486e
Derek Myers <arcticpro@gmail.com>
2019-10-09 12:55:23 -0500

    set a base url for axios requests

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,7 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+window.axios.defaults.baseURL = process.env.MIX_APP_URL;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
