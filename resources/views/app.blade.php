ea8ad8e70aee5a25fdaed5daa5d9a3968e2ba324
Derek Myers <arcticpro@gmail.com>
2019-10-15 01:14:47 -0500

    changed page title to use config var in app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>USCIS Case Tracker</title>
+    <title>{{ config('app.name') }}</title>
     @include('favicon')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script>
